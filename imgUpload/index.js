707fb512df2f8f395008e5ca6534fd4379c3ef3b
SamInfine8 <sameer8creative.tech@gmail.com>
2025-07-15 22:47:02 +0530

    Basic Set-Up

--- imgUpload/index.js
+++ imgUpload/index.js
@@ -1,8 +1,18 @@
-const express = require("express");
-const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 
-app.listen(()=>{
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+
+app.get("/home",(req,res)=>{
+    res.render("home.ejs");
+})
+
+app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
